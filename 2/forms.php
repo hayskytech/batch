0898bf381c85fd61ebf1937210993800cb1a3679
sufyan <mdsufyan7@gmail.com>
2023-03-16 08:59:12 +0530

    Create forms.php

---
+++ 2/forms.php
@@ -0,0 +1,178 @@
+<form>
+	<label>First Name</label>	
+	<input type="text" name="fname">
+	<br><br>
+	
+	<label>Last Name</label>	
+	<input type="text" name="lname">
+	<br><br>
+
+	<label>Gender:</label>	
+	<select name="gender">
+		<option>Male</option>
+		<option>Female</option>
+	</select>
+	<br><br>
+
+	<label>Branch:</label>	
+	<select name="branch">
+		<option>EEE</option>
+		<option>CSE</option>
+		<option>MECH</option>
+		<option>CIVIL</option>
+		<option>ECE</option>
+	</select>
+	<br><br>
+
+	<label>Main Phone:</label>
+	<input type="text" name="phone1">
+	<br><br>
+
+	<label>Alternate Phone:</label>
+	<input type="text" name="phone2">
+	<br><br>
+
+	<input type="submit" name="submit" value="Register">
+</form>
+
+
+<hr>
+
+<table border="1">
+	<tr>
+		<td>Hai</td>
+		<td>Hello</td>
+		<td>Byee</td>
+	</tr>
+	<tr>
+		<td>how are you</td>
+		<td>whats up</td>
+		<td>ok bye</td>
+	</tr>
+	<tr>
+		<td>asbdasdli</td>
+		<td>asbdasdli</td>
+		<td>ilsal d7as di</td>
+	</tr>
+</table>
+<br>
+
+
+<table border="1">
+	<tr>
+		<td></td>
+		<td>1</td>
+		<td>2</td>
+		<td>3</td>
+		<td>4</td>
+		<td>5</td>
+		<td>6</td>
+		<td>7</td>
+		<td>8</td>
+	</tr>
+	<tr>
+		<td>Monday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Tuesday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Wednesday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Thursday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Friday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Saturday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+</table>
+
+
+<h1>Order Your Product</h1>
+
+<form>
+	
+<table>
+	<tr>
+		<td>First Name</td>
+		<td><input type="text" name="fname"></td>
+	</tr>
+	<tr>
+		<td>Last Name</td>
+		<td><input type="text" name="fname"></td>
+	</tr>
+	<tr>
+		<td>Date of Birth</td>
+		<td><input type="date" name="dob"></td>
+	</tr>
+	<tr>
+		<td>Email</td>
+		<td><input type="email" name="email"></td>
+	</tr>
+	<tr>
+		<td>Password</td>
+		<td><input type="color" name="pwd"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><button>Order Now</button></td>
+	</tr>
+</table>
+
+
+</form>
+
+Input Types: color, password, number, range, checkbox
+
+<br><br><br><br><br><br><br><br><br>
