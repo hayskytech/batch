65a74341114ac6d597e15e0c39d1367ea6f0e221
sufyan <mdsufyan7@gmail.com>
2023-03-16 09:05:51 +0530

    Create tables.php

---
+++ 2/tables.php
@@ -0,0 +1,99 @@
+<table border="1">
+	<tr>
+		<td>Hai</td>
+		<td>Hello</td>
+		<td>Byee</td>
+	</tr>
+	<tr>
+		<td>how are you</td>
+		<td>whats up</td>
+		<td>ok bye</td>
+	</tr>
+	<tr>
+		<td>asbdasdli</td>
+		<td>asbdasdli</td>
+		<td>ilsal d7as di</td>
+	</tr>
+</table>
+
+<br>
+
+<table border="1">
+	<tr>
+		<td></td>
+		<td>1</td>
+		<td>2</td>
+		<td>3</td>
+		<td>4</td>
+		<td>5</td>
+		<td>6</td>
+		<td>7</td>
+		<td>8</td>
+	</tr>
+	<tr>
+		<td>Monday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Tuesday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Wednesday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Thursday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Friday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+	<tr>
+		<td>Saturday</td>
+		<td>Telugu</td>
+		<td>Hindi</td>
+		<td>English</td>
+		<td>Maths</td>
+		<td>Science</td>
+		<td>Social</td>
+		<td>PET</td>
+		<td>Computer</td>
+	</tr>
+</table>
