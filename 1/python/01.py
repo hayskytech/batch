824349cb2c873254488d1f1deb4acb18ed526b81
sufyan <mdsufyan7@gmail.com>
2023-05-08 10:32:49 +0530

    basics operations

---
+++ 1/python/01.py
@@ -0,0 +1,20 @@
+print("hello")
+
+print(25 + 5)
+
+# this is error
+# print("2" + 2)
+
+print("hai how are you?")
+
+a = 5
+a = 6
+print(a)
+
+fname = 'Ramesh'
+lname = 'Kumar'
+
+print(fname + ' ' + lname)
+
+age = 20
+print("My age is " + str(age))
