0a0ea2d954380be2c7ae77cc63095acc8434cfec
sufyan <mdsufyan7@gmail.com>
2023-05-09 10:32:29 +0530

    variables

---
+++ 1/python/03.py
@@ -0,0 +1,35 @@
+"""
+Multi line comment:
+lsahdasf
+sadlasdbaksdas
+dsadkkasbldkbasild
+"""
+
+x = True
+y = 6.2
+print( type(x) )
+print( type(y) )
+
+
+student_name = "Ramesh"
+studentAge = 15
+studentnum = "+918498000172"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Apple"
+print("------------------------------")
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+# we can print int and str like this using comma
+x = 5
+y = "John"
+print(x, y)
