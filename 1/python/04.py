0404b8716d149391aaac7d0b3d386fba0459711a
sufyan <mdsufyan7@gmail.com>
2023-05-09 10:33:25 +0530

    loops

---
+++ 1/python/04.py
@@ -0,0 +1,15 @@
+# print 1 to 10 using for loop
+for i in range(1,11):
+	print( str(i) + ") helllo")
+	pass
+
+
+# Print odd numbers
+i = 1
+while i <= 10:
+  print(i)
+  i = i + 2
+  
+n = int(input("Enter table number:"))
+for i in range(1, 11):
+	print(str(n) + " x " + str(i) + " = "+str(n*i))
