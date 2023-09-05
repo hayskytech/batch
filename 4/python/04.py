cd2016e6a6d30bcc7c1fc524e2a633ea9f76802c
sufyan <mdsufyan7@gmail.com>
2023-09-05 10:45:33 +0530

    data types strings

---
+++ 4/python/04.py
@@ -0,0 +1,43 @@
+
+
+
+a = [ 20,23,6,5,68,20 ]
+
+b = ('apple','bat','cat')
+
+c = {'apple','bat','cat'}
+
+n = 20.1
+z = int(n)
+
+k = 30
+l = float(k)
+
+print('temp is ' + str(n))
+
+import random
+print(random.randrange(1, 7))
+
+name = '''kabsdasd as;p d as7i 
+dhasidy asld7 asdu gasld7u 
+gasldu goas7d aso'''
+
+y = 'Haysky'
+print(y[1])
+
+name = 'Dr.Ramu is a good boys'
+print(name[3:])
+print(name[0:-3])
+print(len(name))
+
+a = "Hello, World! Hai"
+print(a.replace("H", "J"))
+
+txt = "Sun rises in the east"
+
+x = "apple" in txt
+print(x)
+
+
+x = "I want {} pieces of itemno. {} for {} dollars."
+print(x.format(3, 1, 350))
